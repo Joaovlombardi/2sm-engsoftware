6805be4d01bcc4fc17e8ec8e71048e4445f83283
João Vitor <131156937+Joaovlombardi@users.noreply.github.com>
2025-08-19 21:40:09 -0300

    Update script7.py
    mudança script7 (correção)

--- python/matriz/script7.py
+++ python/matriz/script7.py
@@ -7,6 +7,10 @@ def criar_matriz_zeros(m, n):
         for j in range(n):
             linha.append(0)
 
-        linha [j - i] = 1 # e resultado do número subtraido é a posição do 1
+        linha [j - i] = 1
         matriz.append(linha)
-        print(matriz)
+    return matriz
+
+matriz = criar_matriz_zeros(5, 5)
+for linha in matriz:
+    print(linha)
